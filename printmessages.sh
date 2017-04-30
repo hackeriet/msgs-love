ae18e184551e36a0741d7b25bc1171f707b8266a
Stig Otnes Kolstad <stig@stigok.com>
2017-04-30 20:05:28 +0200

    Fix wrong env var name

--- printmessages.sh
+++ printmessages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MESSAGE_DIR=${MESSAGE_DIR:-"./"}
+MESSAGE_PATH=${MESSAGE_PATH:-"./"}
 
 while true; do
-  for f in ${MESSAGE_DIR}/*
+  for f in ${MESSAGE_PATH}/*
   do
     rounds=0
     while [[ $rounds -lt 777 ]]; do
